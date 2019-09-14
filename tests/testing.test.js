28eb675ac4e01a36ef03d02b54ab6ea5b96ebef8
Qureeb Hameed <quareebs@yahoo.com>
2019-09-14 17:16:21 +0300

    added part 4.5*: helper functions and unit tests, step3

--- tests/testing.test.js
+++ tests/testing.test.js
@@ -18,3 +18,45 @@ describe('total likes', () => {
     expect(result).toBe(5)
   })
 })
+
+describe('favorite blog', () => {
+  blogs = [
+    {
+      _id: '5a422aa71b54a676234d17f8',
+      title: 'Go To Statement Considered Harmful',
+      author: 'Edsger W. Dijkstra',
+      url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+      likes: 5,
+      __v: 0
+    },
+    {
+      _id: "5a422b891b54a676234d17fa",
+      title: "First class tests",
+      author: "Robert C. Martin",
+      url: "http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll",
+      likes: 1,
+      __v: 0
+    },
+    {
+      _id: "5a422bc61b54a676234d17fc",
+      title: "Type wars",
+      author: "Robert C. Martin",
+      url: "http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html",
+      likes: 2,
+      __v: 0
+    }
+  ]
+
+  test('returns blog entry with most likes', () => {
+    const result = listHelper.favoriteBlog(blogs)
+    expect(result).toEqual(
+      {
+        _id: '5a422aa71b54a676234d17f8',
+        title: 'Go To Statement Considered Harmful',
+        author: 'Edsger W. Dijkstra',
+        url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+        likes: 5,
+        __v: 0
+      })
+  })
+})
--- utils/list_helper.js
+++ utils/list_helper.js
@@ -2,8 +2,28 @@ const totalLikes = (blogs) => {
     return blogs.reduce((acc, val) => acc + val.likes, 0)
   }
   
+const favoriteBlog = (blogs) => {
+    let likes = blogs.map((blog) => blog.likes),
+        maxVal = Math.max(...likes),
+        index = likes.indexOf(maxVal)
+
+        return blogs[index]
+}
 
 
   module.exports = {
-    totalLikes
+    totalLikes,
+    favoriteBlog
   }
+
+
+  favoriteBlog([
+      {
+        me:'you',
+        likes: 5
+    },
+    {
+        you:'me',
+        likes: 11
+    }
+])
