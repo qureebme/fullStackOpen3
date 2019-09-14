4d9835c2d474e072e3102646d9bfa2d0b25d9cd3
Qureeb Hameed <quareebs@yahoo.com>
2019-09-14 16:46:26 +0300

    part 4.4: helper functions and unit tests, step2

--- utils/config.js
+++
@@ -1,9 +0,0 @@
-require('dotenv').config()
-
-let port = 3003
-let mongoURI = 'mongodb+srv://qureeb:qureebPassword@cluster0-bmqmg.mongodb.net/blogs?retryWrites=true&w=majority'
-
-module.exports = {
-  mongoURI,
-  port
-}
--- utils/list_helper.js
+++ utils/list_helper.js
@@ -1,9 +1,9 @@
-const dummy = (blogs) => {
-    return 1
+const totalLikes = (blogs) => {
+    return blogs.reduce((acc, val) => acc + val.likes, 0)
   }
   
 
 
   module.exports = {
-    dummy
+    totalLikes
   }
