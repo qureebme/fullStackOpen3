beccf8b0322899c21a1aae9e876a77470b71fe35
Qureeb Hameed <quareebs@yahoo.com>
2019-09-21 14:08:44 +0300

    dunno if it's neccessary to commit this .test.js

--- tests/testing.test.js
+++ tests/testing.test.js
@@ -60,3 +60,95 @@ describe('favorite blog', () => {
       })
   })
 })
+
+describe('blogger with most blogs', () =>{
+  list = [
+    {
+      _id: '5a422aa71b54a676234d17f8',
+      title: 'Go To Statement Considered Harmful',
+      author: 'Robert C. Martin',
+      url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+      likes: 5,
+      __v: 0
+    },
+    {
+      _id: "5a422b891b54a676234d17fa",
+      title: "First class tests",
+      author: "Robert C. Martin",
+      url: "http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll",
+      likes: 1,
+      __v: 0
+    },
+    {
+      _id: "5a422bc61b54a676234d17fc",
+      title: "Type wars",
+      author: "Edsger W. Dijkstra",
+      url: "http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html",
+      likes: 2,
+      __v: 0
+    }
+  ]
+  
+
+  test('returns blogger and number of blogs', () => {
+    const result = listHelper.mostBlogs(list)
+    
+    expect(result).toEqual(
+      {
+        author: "Robert C. Martin",
+        blogs: 2
+      }
+    )
+
+  })
+})
+
+describe('blogger with most likes', () =>{
+  list = [
+    {
+      _id: '5a422aa71b54a676234d17f8',
+      title: 'Go To Statement Considered Harmful',
+      author: 'Edsger W. Dijkstra',
+      url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+      likes: 5,
+      __v: 0
+    },
+    {
+      _id: "5a422b891b54a676234d17fa",
+      title: "First class tests",
+      author: "Robert C. Martin",
+      url: "http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll",
+      likes: 4,
+      __v: 0
+    },
+    {
+      _id: "5a422bc61b54a676234d17fc",
+      title: "Type wars",
+      author: "Robert C. Martin",
+      url: "http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html",
+      likes: 2,
+      __v: 0
+    },
+    {
+      _id: "5a422bc61b54a676234d17fc",
+      title: "Type wars",
+      author: "qureeb",
+      url: "http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html",
+      likes: 11,
+      __v: 0
+    }
+  ]
+  
+
+  test('returns blogger and total likes', () => {
+    const result = listHelper.mostLikes(list)
+    
+    expect(result).toEqual(
+      {
+        author: "qureeb",
+        likes: 11
+      }
+    )
+
+  })
+})
