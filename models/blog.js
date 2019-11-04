da0544406a6f806ba4d1a4edc3a8d63307413f95
Qureeb Hameed <quareebs@yahoo.com>
2019-11-04 14:06:24 +0200

    added 4.9*: Blog list tests, step2

--- models/blog.js
+++ models/blog.js
@@ -5,8 +5,16 @@ const blogSchema = mongoose.Schema({
     author: String,
     url: String,
     likes: Number
-  })
-  
-  const Blog = mongoose.model('Blog', blogSchema)
+})
 
-  module.exports = Blog
+blogSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
+})
+
+const Blog = mongoose.model('Blog', blogSchema)
+
+module.exports = Blog
--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -6,13 +6,19 @@ const api = supertest(app)
 
 describe('api tests', () => {
 
-    test.only('Blog returns the correct number of blog posts in json format', async () => {
+    test('Blog returns the correct number of blog posts in json format', async () => {
         let results = await api
             .get('/api/blogs')
             .expect(200)
             .expect('Content-Type', /application\/json/)
 
             expect(results.body.length).toBe(11)
+    })
+
+    test.only('Expect id to be defined', async () => {
+        let results = await api
+                .get('/api/blogs')
+            results.body.map((item) => {expect(item.id).toBeDefined()})
     })
 })
 
