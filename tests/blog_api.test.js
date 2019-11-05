33777503fc061af0b4599216896bf4256059d081
Qureeb Hameed <quareebs@yahoo.com>
2019-11-05 13:29:58 +0200

    added 4.10: Blog list tests, step3

--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose')
 const supertest = require('supertest')
 const app = require('../app')
+const Blog = require('../models/blog')
 
 const api = supertest(app)
 
@@ -15,10 +16,30 @@ describe('api tests', () => {
             expect(results.body.length).toBe(11)
     })
 
-    test.only('Expect id to be defined', async () => {
+    test('Expect id to be defined', async () => {
         let results = await api
                 .get('/api/blogs')
             results.body.map((item) => {expect(item.id).toBeDefined()})
+    })
+
+    test.only('Post request succesfully creates new blog post', async () => {
+        let blogEntry = {
+            title: 'test blog2',
+            author: 'tyson gay',
+            url: 'www.2die4.com',
+            likes: 8
+        }
+        let get1 = await api.get('/api/blogs').expect(200),
+            initialNumber = get1.body.length;
+
+        let post1 = await api.post('/api/blogs')
+                            .send(blogEntry)
+                            .expect(201)
+
+        let get2 = await api.get('/api/blogs').expect(200),
+            finalNumber = get2.body.length;
+
+        expect(finalNumber).toBe(initialNumber + 1)
     })
 })
 
