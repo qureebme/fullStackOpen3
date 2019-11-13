9f8f24d04c7c2aa341ba6a8be4e2fc9364b5b357
Qureeb Hameed <quareebs@yahoo.com>
2019-11-13 13:20:53 +0200

    edited router to ensure user provides a name

--- controllers/userRouter.js
+++ controllers/userRouter.js
@@ -6,7 +6,12 @@ const bcrypt = require('bcryptjs')
 userRouter.post('/', async (req, res, next) => {
     try {
         const body = req.body
-
+        if (!body.username || !body.password){
+            return res.status(400).send('username and password must be given')
+        }
+        if (!(body.username.length > 2) || !(body.password.length > 2) || !(body.name)){
+            return res.status(400).send('username or password is too short')
+        }
         const saltRounds = 10
         const passwordHash = await bcrypt.hash(body.password, saltRounds)
 
