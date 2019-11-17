5939ceb5a12688832e3fe4a77f1d5c688c8dc922
Qureeb Hameed <quareebs@yahoo.com>
2019-11-17 21:19:23 +0200

    added 4.18: bloglist expansion, step7

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
 const blogRouter = require('./controllers/router')
 const userRouter = require('./controllers/userRouter')
+const loginRouter = require('./controllers/login')
 
 const mongoURI = config.MONGODB_URI
 mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
@@ -16,12 +17,13 @@ mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
 app.use(cors())
 app.use(bodyParser.json())
 
+app.use('/api/login', loginRouter)
 app.use('/api/users', userRouter)
 app.use('/api/blogs', blogRouter)
 
 
 const errorHandler = (err, req, res, next) => {
-        if (err.name == "ValidationError") res.status(400).send('username must be unique')
+        if (err.name == "ValidationError") res.status(400).send({error: 'username must be unique'})
         else res.status(500).send('Server error')
     }
 
